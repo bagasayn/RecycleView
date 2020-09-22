017d235b2bfd6371754f930cfa5b173d839a67bd
bagasayn <bagasayn@gmail.com>
2020-09-22 11:12:53 +0300

    Job version

--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numbersList: RecyclerView = findViewById(R.id.rv_numbers) // создаем обект для Kotlin
+
+        val layoutManager = LinearLayoutManager(this)
+        numbersList.layoutManager = layoutManager // передаем содержимое списка
+
+        numbersList.setHasFixedSize(true) // заранее говорим, что знаем размер списка, ускоряет работу
+
+        val numbersAdaptor = NumbersAdaptor(100)
+        numbersList.adapter = numbersAdaptor
+
     }
 }
---
+++ app/src/main/java/com/example/recycleview/NumbersAdaptor.kt
@@ -0,0 +1,45 @@
+package com.example.recycleview
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NumbersAdaptor(numberOfItems: Int, private var viewHolderCount: Int = 0) : RecyclerView.Adapter<NumbersAdaptor.NumberViewHolder>() {
+    private var numberItems : Int = numberOfItems
+
+
+    class NumberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var listItemNumberView: TextView = itemView.findViewById(R.id.tv_number_item)       // TextView 1 список чисел
+        var viewHolderIndex: TextView = itemView.findViewById(R.id.tv_view_holder_number)   // TextView 2 список индексов
+
+        fun bind(listIndex: Int){
+            listItemNumberView.text = listIndex.toString()
+        }                                                       // Меняем значения TextView 1 в диапозоне 1-100
+
+    } // оборачиваем элемент списка и передаем новые значения при прокрутке списка
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        val context: Context = parent.context
+        val layoutIdForListItem : Int = R.layout.number_list_item   // передаем ID XML файла
+        val inflater: LayoutInflater = LayoutInflater.from(context) // создаем объект класса View из XML файла
+        val view: View = inflater.inflate(layoutIdForListItem,parent,false) // Создаем новый элемент из списка XML. 1-Указываем из какого XML мы хотим создать новый объект. 2-Задаем родителя. 3-Разрешение на помещение нового элемента внутрь родителя
+        val viewHolder = NumberViewHolder(view)   // Оборачивание Элемента из списка для переиспользования его в дальнейшем
+        viewHolder.viewHolderIndex.text =
+            "ViewHolder index: $viewHolderCount"  // Вводим новый текст значения TextView 2
+        viewHolderCount++
+        return viewHolder
+    } // Создаем новые ViewHolder-ы
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.bind(position)
+    }              // У созданных ViewHolder-ов меняем значения при прокрутке
+
+    override fun getItemCount(): Int {
+        return numberItems
+    }                                                    // Общее количество элементов в списке
+}
